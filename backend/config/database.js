816279d19343d7bd131648d0cd26c8c1ec3d9a6a
Thiago da Silva <thiggasto@gmail.com>
2026-05-28 16:01:02 -0300

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize")
-require("dotenv").config()
-
-const sequelize = new Sequelize({
- dialect: "sqlite",
- storage: process.env.DB_PATH
-})
-
-module.exports = sequelize
